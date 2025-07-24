603ac24feea7c8a4ee48d4921db6da5c37990666
Sharjeel Ahmad <156166566+SharjeelAhmad-12@users.noreply.github.com>
2025-07-24 09:43:33 +0500

    refactor:gradient issue in service and success section

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -48,7 +48,7 @@ const Home: React.FC = () => {
       <TestimonialCard />
       <div className="relative overflow-hidden">
         <div
-          className="absolute w-[40rem] h-[30rem] bottom-0 left-[-10rem] rounded-tr-[100%] filter blur-3xl opacity-50 z-0"
+          className="absolute w-[40rem] h-[30rem] bottom-0 left-[-10rem] rounded-tr-[100%] filter blur-3xl opacity-40 z-0"
           style={{
             background: "linear-gradient(to right, #78BA9F 80%, #12334E -20%)",
             animation: "blob 8s infinite",
